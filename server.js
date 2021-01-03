2226ac0e072dc9fa996ce74c5cd0a29265e10b1d
Phan Văn Tuấn <phanvantuan99hth@gmail.com>
2021-01-03 11:29:43 +0700

    abc xyz

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000; // Step 1
+
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "quanlygiaothongweb", "build")))
+    app.use(express.static( 'quanlygiaothongweb/build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'quanlygiaothongweb', 'build', 'index.html')); // relative path
+    });
+}
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
