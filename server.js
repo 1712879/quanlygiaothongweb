9a573e417a5c81dda0e86f735928bfae09cc8280
Phan Văn Tuấn <phanvantuan99hth@gmail.com>
2021-01-09 13:40:48 +0700

    modify

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ require('dotenv').config()
 const port = process.env.PORT || 5000; // Step 1
 
 const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "quanlygiaothongweb", "build")))
-    app.use(express.static( 'quanlygiaothongweb/build' ));
+app.use(express.static(path.join(__dirname, "quanlygiaothongweb", "build")))
+app.use(express.static( 'quanlygiaothongweb/build' ));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'quanlygiaothongweb', 'build', 'index.html')); // relative path
-    });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'quanlygiaothongweb', 'build', 'index.html')); // relative path
+});
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
