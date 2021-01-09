34dfe3a4c27481a58e86811ce71893b4d75cb7e4
Phan Văn Tuấn <phanvantuan99hth@gmail.com>
2021-01-09 14:03:30 +0700

    add build

---
+++ quanlygiaothongweb/build/precache-manifest.764765bf80258d77f683ddc17c3279d0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdef9a7e385b6884e48f5771941878d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d44471baf7ba25ed81",
+    "url": "/static/css/2.6104a041.chunk.css"
+  },
+  {
+    "revision": "1ea34f5b4c4e9ca00b98",
+    "url": "/static/css/main.5ee3f738.chunk.css"
+  },
+  {
+    "revision": "b8d44471baf7ba25ed81",
+    "url": "/static/js/2.c345f55a.chunk.js"
+  },
+  {
+    "revision": "a126c8cf0b2ae300771065c176cb644a",
+    "url": "/static/js/2.c345f55a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea34f5b4c4e9ca00b98",
+    "url": "/static/js/main.39c9fd91.chunk.js"
+  },
+  {
+    "revision": "9d1948da6512e239c4cd",
+    "url": "/static/js/runtime-main.f9fa5b6e.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
